e7871b29c4664b497801ebd6e054978833e0d819
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2023-03-13 14:45:57 -0300

    commit 7DaysOfCode de javascript iniciado
    aqui eu preparei alguns arquivos e ja fiz o dia 1 e 2 do desafio.

---
+++ 7DaysOfCodeJavaScript/main.js
@@ -0,0 +1,45 @@
+/*Exercicio do primeiro dia*/
+let numeroUm = 1;
+let stringUm = '1';
+let numeroTrinta = 30;
+let stringTrinta = '30';
+let numeroDez = 10;
+let stringDez = '10';
+
+if (numeroUm == stringUm) {
+    console.log("As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes.");
+} else {
+    console.log('As variáveis numeroUm e stringUm não tem o mesmo valor');
+}
+
+if (numeroTrinta === stringTrinta) {
+    console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+  } else {
+    console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+  }
+
+  if (numeroDez == stringDez) {
+    console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+  } else {
+    console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+  }
+
+  /*Exercicio do segundo dia*/
+
+  const nome = prompt("Olá, primeiramente qual o seu nome?");
+  const  idade = prompt(`É um prazer ${nome} vamos continuar, quantos anos você tem?`);
+  const aprendendoLinguagem = prompt(`${nome}, vou te fazer mais uma pergunta, Qual linguagem de programação você está estutando? `);
+  var apresentacao = `Olá ${nome}, você tem ${idade} anos e já está aprendendo ${aprendendoLinguagem}!`
+
+  const res = document.querySelector(".escreveTela");
+  res.innerHTML = apresentacao;
+
+  const resposta = prompt(`Você gosta de estudar ${aprendendoLinguagem}? Responda com o número 1 para SIM ou 2 para NÃO.`);
+
+    if (resposta == 1) {
+        res.innerHTML = apresentacao + " Muito bom! Continue estudando e você terá muito sucesso.";
+    } 
+    if (resposta == 2) {
+        res.innerHTML = apresentacao + " Ahh que pena... Já tentou aprender outras linguagens?";
+        
+    } 
